23626ee45e97a6187bc19604da6c1dbb2bed79d6
pinkipail <fyodorov_vasya@list.ru>
2020-04-28 17:51:34 +0300

    add toggle night mode

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 window.addEventListener('load', function(){
-    const mouse = document.querySelector('#light')
-    document.addEventListener('mousemove', function(event){
-       mouse.style.top = event.pageY - 250 + 'px'
-       mouse.style.left = event.pageX - 250 + 'px'
-               
-    })
+   mouseMove()
+   toggleNightMpde()
  })
  
+
+function mouseMove(){
+   const mouse = document.querySelector('#light')
+   document.addEventListener('mousemove', function(event){
+      mouse.style.top = event.pageY - 250 + 'px'
+      mouse.style.left = event.pageX - 250 + 'px'
+              
+   })
+}
+function toggleNightMpde(){
+   const btnSwitch = document.querySelector('.switch')
+
+   btnSwitch.addEventListener('click', ()=>{
+      document.body.classList.toggle('night')
+   })
+}
